a2d7c6fc69ff65f00be004762e74eb6ecaf5e6bc
marivgil <gil.maricruz@gmail.com>
2018-09-15 23:59:41 -0300

    ejemplo de un select que funciona

--- src/app/components/home-street/home-street.component.ts
+++ src/app/components/home-street/home-street.component.ts
@@ -4,11 +4,30 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-home-street',
   templateUrl: './home-street.component.html'
 })
+
 export class HomeStreetComponent implements OnInit {
 
+  round;
+  rounds = [];
   constructor() { }
 
+  states = [
+    {name: 'Arizona', abbrev: 'AZ'},
+    {name: 'California', abbrev: 'CA'},
+    {name: 'Colorado', abbrev: 'CO'},
+    {name: 'New York', abbrev: 'NY'},
+    {name: 'Pennsylvania', abbrev: 'PA'},
+  ];
+
+
   ngOnInit() {
+    this.getRounds()
   }
+
+  getRounds(){
+
+  }
+
+
 
 }
