19b6e20d4653a846f4f4f8ce6baff60bde821e00
marivgil <gil.maricruz@gmail.com>
2018-09-16 16:36:08 -0300

    Comienzo del armado de pedidos en calle

---
+++ src/app/app.consts.ts
@@ -0,0 +1,43 @@
+
+export const ROUNDS = [
+  'Recorrido 1',
+  'Recorrido 2',
+  'Recorrido 3',
+  'Recorrido 4',
+  'Recorrido 5',
+  'Recorrido 6',
+  'Recorrido 7',
+  'Recorrido 8',
+  'Recorrido 9',
+  'Recorrido 10',
+  'Recorrido 11',
+  'Recorrido 12',
+  'Recorrido 13',
+  'Recorrido 14',
+  'Recorrido 15',
+  'Recorrido 16',
+  'Recorrido 17',
+  'Recorrido 18',
+  'Recorrido 19',
+  'Recorrido 20'
+];
+
+export const NAMECLOTHESMAN = [
+  'Pantalón','Remera','Camisa','Sweater','Campera','Medias','Guantes',
+  'Gorros','Calzoncillos','Canzoncillos largos','Bufanda','Zapatillas','Zapatos'
+];
+
+export const NAMECLOTHESWOMAN = [
+  'Pantalón','Calza','Remera','Camisa','Sweater','Campera','Medias','Guantes',
+  'Gorros','Bombachas','Corpiño','Bufanda','Zapatillas','Zapatos'
+];
+
+export const GENDER = [
+  'Mujer', 'Hombre', 'Nena', 'Nene'
+];
+
+export const WAIST = [
+  'XXS','XS','S','M','L','XL','XXL',
+  '32','34','36','38','40','42','44','46','48','50','52','54','56','58','60','62','64',
+  '0','2','4','6','8','10','12',
+];
