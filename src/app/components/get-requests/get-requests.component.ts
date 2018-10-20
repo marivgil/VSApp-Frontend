e9cac850454063ebee8d40e4792889398b81e298
marivgil <gil.maricruz@gmail.com>
2018-10-19 22:24:53 -0300

    se corrige consulta al servicio para que traiga el pedido para sede seleccionado

--- src/app/components/get-requests/get-requests.component.ts
+++ src/app/components/get-requests/get-requests.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit, ViewContainerRef} from '@angular/core';
 import {GetRequestsService} from "./get-requests.service";
 import {ToastsManager} from "ng2-toastr";
+import {WeeklyRound} from "../../interfaces/WeeklyRound";
 
 @Component({
   selector: 'app-get-requests',
@@ -9,8 +10,7 @@ import {ToastsManager} from "ng2-toastr";
 export class GetRequestsComponent implements OnInit {
 
   rounds;
-  weeklyRound = '';
-  request;
+  weeklyRound: WeeklyRound = null;
   clothes = [];
   c;
   rc;
@@ -33,21 +33,23 @@ export class GetRequestsComponent implements OnInit {
   }
 
   searchRequest(){
-    if (this.weeklyRound == ''){
+    if (this.weeklyRound == null){
       this.toastr.error('Debe seleccionar un recorrido');
     } else {
       this.getRequest = true;
       this.firstEntry=false;
+      /*
       this.service.searchRequest(this.weeklyRound).subscribe(result => {
         this.request = result;
         if(this.request == null){
           this.clothes = [];
         }else{
-          this.clothes = this.request.clothes;
-          this.preparedBy = this.request.preparedBy;
-          this.reviewedBy = this.request.reviewedBy;
-        }
-          });
+        */
+          this.clothes = this.weeklyRound.request.clothes;
+          this.preparedBy = this.weeklyRound.request.preparedBy;
+          this.reviewedBy = this.weeklyRound.request.reviewedBy;
+       // }
+       //   });
     }
   }
 
