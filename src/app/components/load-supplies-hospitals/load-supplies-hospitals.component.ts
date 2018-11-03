62a81c57690fb5a5e8bab3ca99a84a85e1fa1851
marivgil <gil.maricruz@gmail.com>
2018-11-03 00:14:21 -0300

    - fix #10 solo para el front - se  agrega paginación al pedido de calle

--- src/app/components/load-supplies-hospitals/load-supplies-hospitals.component.ts
+++ src/app/components/load-supplies-hospitals/load-supplies-hospitals.component.ts
@@ -32,18 +32,24 @@ export class LoadSuppliesHospitalsComponent implements OnInit {
   }
 
   addProduct(){
-    if(this.hospital=='' || this.prodType==''||this.nameProduct=='' || this.quantityProd<=0 || this.quantityProd==null){
-      this.toastr.error('Faltan completar datos para dar de alta el producto');
-    }else{
-      let product = {
-        "hospital": this.hospital,
-        "type": this.prodType,
-        "name": this.nameProduct,
-        "quantity": this.quantityProd
-      };
-      this.service.addProduct(product).subscribe();
-      this.toastr.success('El producto fue dado de alta');
-    }
+    if(this.hospital=='')
+      this.toastr.error('Te falta seleccionar el nombre del hospita');
+      else if(this.prodType=='')
+            this.toastr.error('Te faltan completar el tipo de producto');
+            else if(this.nameProduct=='')
+                  this.toastr.error('Te faltan agregar el nombre del producto');
+                  else if(this.quantityProd<=0 || this.quantityProd==null)
+                      this.toastr.error('Por favor, agregá una cantidad válida');
+                      else{
+                        let product = {
+                          "hospital": this.hospital,
+                          "type": this.prodType,
+                          "name": this.nameProduct,
+                          "quantity": this.quantityProd
+                        };
+                        this.service.addProduct(product).subscribe();
+                        this.toastr.success('El producto fue dado de alta');
+                      }
     this.firstEntry = true;
     this.nameProduct = '';
     this.quantityProd = 0;
@@ -51,12 +57,13 @@ export class LoadSuppliesHospitalsComponent implements OnInit {
     let date = new Date();
     let month:number = date.getMonth()+1; // WTF????
 
+    /*
     this.service.getProductsHospitalByDay(
       date.getFullYear()+""+month+""+date.getDate()).
     subscribe(res => {
       this.hospitalProducts = res;
     });
-
+*/
   }
 
 }
