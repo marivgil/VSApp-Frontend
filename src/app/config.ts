3feeddb161a17f4f6791bf62f131c1b0b9fce350
marivgil <gil.maricruz@gmail.com>
2019-07-23 23:44:56 -0300

    probando cors desde el front

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,6 +1,6 @@
 export const URL_BACKEND_HOMO = "http://localhost:8080/VSApp-BackEnd/rest/";
 
-export const URL_BACKEND = "https://vsapp-backend.herokuapp.com/rest";
+export const URL_BACKEND = "https://vsapp-backend.herokuapp.com/rest/";
 
 export const URL_FRONTEND_HOMO = "http://localhost:4200/";
 
