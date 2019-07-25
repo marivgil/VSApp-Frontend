8de1c811d699fca3409891dabf2a891bf98c5cd2
marivgil <gil.maricruz@gmail.com>
2019-07-25 20:54:04 -0300

    probando cors desde el front

--- server.js
+++ server.js
@@ -36,3 +36,9 @@ const path = require('path');
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
