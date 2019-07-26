bdd37a8e565b1211a002d8026246c7cb70a713d7
marivgil <gil.maricruz@gmail.com>
2019-07-25 21:11:37 -0300

    probando cors desde el front

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {URL_BACKEND} from "./config";
-import {Http} from "@angular/http";
+//import {Http} from "@angular/http";
+import {HttpHeaders, HttpClient} from "@angular/common/http";
 
 @Injectable()
 export class UserService {
@@ -8,11 +9,18 @@ export class UserService {
   userProfile;
   extensionUrl: String = "servicesUsers/";
 
-  constructor(private http: Http) { }
+  constructor(private http: HttpClient) { }
 
   getUser(mail: string){
+
+    const headers = new HttpHeaders()
+      .append('Content-Type', 'application/json')
+      .append('Access-Control-Allow-Headers', 'Content-Type')
+      .append('Access-Control-Allow-Methods', 'GET')
+      .append('Access-Control-Allow-Origin', '*');
+
     return this.http.get(
-      URL_BACKEND +  this.extensionUrl + "findUserByMail/" + mail);
+      URL_BACKEND +  this.extensionUrl + "findUserByMail/" + mail,{headers: headers});
   }
 
   userNoExist() {
