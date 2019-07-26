e0dc03a41aa5ffcd413382bb0072e3b6c29410e2
marivgil <gil.maricruz@gmail.com>
2019-07-25 21:06:42 -0300

    probando cors desde el front

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/dist'));
+//app.use(express.static(__dirname + '/dist'));
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
@@ -39,6 +39,7 @@ app.get('/*', function(req, res) {
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "Origin, " +
+    "X-Requested-With, Content-Type, Accept");
   next();
 });
